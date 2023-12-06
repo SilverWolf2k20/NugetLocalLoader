3564060104bb2d3f91ca549e3fbfd229136d7c42
Ihor <egorwolf2k18@mail.ru>
2023-12-06 21:09:10 +0300

    feat: добавлена работа с командами
    Добавлены аргументы --help, --version и подкоманда find

--- src/OkoloIt.NugetLocalLoader.Cli/Program.cs
+++ src/OkoloIt.NugetLocalLoader.Cli/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using CommandDotNet;
+
+[Command(
+    Description = "NugetLocalLoader is a program to load Nuget packages into a local folder with all dependencies.")]
+public class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        return await new AppRunner<Program>()
+            .UseVersionMiddleware()
+            .RunAsync(args);
+    }
+
+    [Subcommand(RenameAs ="find")]
+    public Finder Finder { get; set; } = null!;
+}
+
+public class Finder
+{
+    [DefaultCommand]
+    public static void FinderImpl(IConsole console) => console.WriteLine("find");
+}
+
+// https://commanddotnet.bilal-fazlani.com/gettingstarted/getting-started-300-help/
