47f0658e5b0d8ade875bb510fef9db13313d30c5
egorfilatov200011@gmail.com <egorfilatov200011@gmail.com>
2023-12-16 14:10:11 +0300

    feat: добавлено описание команды

--- src/OkoloIt.NugetLocalLoader.Cli/Program.cs
+++ src/OkoloIt.NugetLocalLoader.Cli/Program.cs
@@ -1,11 +1,20 @@
 ﻿using CommandDotNet;
 
-using OkoloIt.NugetLocalLoader.Core;
+using OkoloIt.NugetLocalLoader.Cli;
 
 [Command(
     Description = "NugetLocalLoader is a program to load Nuget packages into a local folder with all dependencies.")]
 public class Program
 {
+    #region Public Properties
+
+    [Subcommand(RenameAs = "find")]
+    public Finder Finder { get; set; } = new();
+
+    #endregion Public Properties
+
+    #region Public Methods
+
     public static async Task<int> Main(string[] args)
     {
         return await new AppRunner<Program>()
@@ -13,19 +22,5 @@ public class Program
             .RunAsync(args);
     }
 
-    [Subcommand(RenameAs ="find")]
-    public Finder Finder { get; set; } = new();
-}
-
-public class Finder
-{
-    [Command("versions")]
-    public void FindVersions(IConsole console, string packageName)
-    {
-        PackageManager packageManager = new PackageManager();
-        IEnumerable<string> versions = packageManager.GetAllPackageVersions(packageName).Result;
-
-        foreach (string version in versions)
-            console.WriteLine(version);
-    }
+    #endregion Public Methods
 }
