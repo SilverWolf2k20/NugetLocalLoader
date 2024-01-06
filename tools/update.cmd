a8b5e15af203af5a00c9b4903bd0c78a3387cdf4
Ihor <egorwolf2k18@mail.ru>
2024-01-06 16:38:19 +0300

    chore: добавлена установка предварительных версий

--- tools/update.cmd
+++ tools/update.cmd
@@ -2,4 +2,4 @@
 :: Скрипт обновления инструмента.
 :: ============================================================================
 
-dotnet tool update llnuget --global --add-source ..\src\OkoloIt.NugetLocalLoader.Cli\nupkg
+dotnet tool update llnuget --global --prerelease --add-source ..\src\OkoloIt.NugetLocalLoader.Cli\nupkg
