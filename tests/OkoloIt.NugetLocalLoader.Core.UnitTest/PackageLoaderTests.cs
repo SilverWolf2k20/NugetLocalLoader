4814e47ffd3972bcb1947eb8b2b5cb76b8759615
Ihor <egorwolf2k18@mail.ru>
2024-01-14 20:48:07 +0300

    tests: добавлены тесты для загрузчика пакетов

---
+++ tests/OkoloIt.NugetLocalLoader.Core.UnitTest/PackageLoaderTests.cs
@@ -0,0 +1,53 @@
+﻿namespace OkoloIt.NugetLocalLoader.Core.UnitTest;
+
+[TestFixture]
+internal sealed class PackageLoaderTests
+{
+    [Test]
+    public void LoadPackageAsync_IncorrectPackageName_Exception()
+    {
+        // Arrange
+        PackageLoader packageLoader = new();
+
+        // Assert
+        Assert.That(
+            () => packageLoader.LoadPackageAsync(
+                string.Empty,
+                string.Empty,
+                string.Empty,
+                CancellationToken.None).Wait(),
+            Throws.Exception);
+    }
+
+    [Test]
+    public void LoadPackageAsync_IncorrectPackageVersion_Exception()
+    {
+        // Arrange
+        PackageLoader packageLoader = new();
+
+        // Assert
+        Assert.That(
+            () => packageLoader.LoadPackageAsync(
+                "NLog",
+                string.Empty,
+                string.Empty,
+                CancellationToken.None).Wait(),
+            Throws.Exception);
+    }
+
+    [Test]
+    public void LoadPackageAsync_IncorrectPath_Exception()
+    {
+        // Arrange
+        PackageLoader packageLoader = new();
+
+        // Assert
+        Assert.That(
+            () => packageLoader.LoadPackageAsync(
+                "NLog",
+                "6.0.0",
+                string.Empty,
+                CancellationToken.None).Wait(),
+            Throws.Exception);
+    }
+}
