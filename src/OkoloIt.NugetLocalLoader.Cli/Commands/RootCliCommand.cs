868796b40d2a163ce7b1e4a5b29b09113adf48a5
Ihor <egorwolf2k18@mail.ru>
2024-01-16 22:01:45 +0300

    feat: добавлено отображение цвета

--- src/OkoloIt.NugetLocalLoader.Cli/Commands/RootCliCommand.cs
+++ src/OkoloIt.NugetLocalLoader.Cli/Commands/RootCliCommand.cs
@@ -1,7 +1,10 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Drawing;
 
 using DotMake.CommandLine;
+
+using Pastel;
 
 namespace OkoloIt.NugetLocalLoader.Cli.Commands;
 
@@ -12,14 +15,16 @@ public class RootCliCommand
 
     public void Run(InvocationContext context)
     {
-        context.Console.WriteLine("<Okolo IT>\n");
+        context.Console.WriteLine($"Developed <Okolo IT>");
         context.Console.WriteLine("""
-            ╔╗  ╔╗    ╔═╦╗ ╔╗╔╗ ╔══╗ ╔══╗ ╔══╗
-            ║║  ║║    ║║║║ ║║║║ ║╔═╣ ║ ═╣ ╚╗╔╝
-            ║╚╗ ║╚╗   ║║║║ ║╚╝║ ║╚╗║ ║ ═╣  ║║
-            ╚═╝ ╚═╝   ╚╩═╝ ╚══╝ ╚══╝ ╚══╝  ╚╝
-            """);
-        context.Console.WriteLine("\nHello, this is a NugetLocalLoader!\n");
+             _     _     _   _ _   _  ____ _____ _____ 
+            | |   | |   | \ | | | | |/ ___| ____|_   _|
+            | |   | |   |  \| | | | | |  _|  _|   | |
+            | |___| |___| |\  | |_| | |_| | |___  | |
+            |_____|_____|_| \_|\___/ \____|_____| |_|
+            """.Pastel(Color.FromArgb(0, 72, 128)));
+        context.Console.WriteLine("\nHello, this is a NugetLocalLoader!");
+        context.Console.WriteLine("URL: https://github.com/SilverWolf2k20/NugetLocalLoader");
     }
 
     #endregion Public Methods
--- src/OkoloIt.NugetLocalLoader.Cli/Program.cs
+++ src/OkoloIt.NugetLocalLoader.Cli/Program.cs
@@ -2,6 +2,9 @@
 
 using OkoloIt.NugetLocalLoader.Cli.Commands;
 
+using Pastel;
+
+using System.Drawing;
 using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.*")]
@@ -10,9 +13,9 @@ try {
     await Cli.RunAsync<RootCliCommand>(args);
 }
 catch (OperationCanceledException) {
-    Console.WriteLine("The command was canceled.");
+    Console.WriteLine("The command was canceled.".Pastel(Color.FromArgb(241, 210, 46)));
 }
 catch (Exception ex) {
-    Console.Error.WriteLine("Unregistered exception.");
-    Console.Error.WriteLine(ex);
+    Console.Error.WriteLine("Unregistered exception.".Pastel(Color.FromArgb(158, 45, 34)));
+    Console.Error.WriteLine(ex.ToString().Pastel(Color.FromArgb(158, 45, 34)));
 }
